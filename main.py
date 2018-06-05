c49e581bf87cbc12e3f518b6910231a3a6874cb9
SIRomaniejko <siromaniejko@gmail.com>
2018-06-05 20:03:59 -0300

    all files

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-sup
+# coding=utf-8
+
+from naves import *
+from probabilidades import *
+
+print('')
+print('')
+print('Estamos volando en la tranquilidad del espacio.....')
+print('')
+print('')
+print('             __');
+print('             \ \_____');
+print('          ---[==_____>');
+print('             /_/');
+print('')
+print('')
+
