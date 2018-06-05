8b7f38a5cfe84a454f35e6f4c094249c144ac2f8
Terminal 5 VDI Lab III <Terminal 5 VDI Lab III>
2018-06-05 20:33:37 -0300

    ultimo

--- naves.py
+++ naves.py
@@ -1,6 +1,12 @@
-naveA = 10
-naveB = 15
-naveC = 15
-naveAProb = 0.1
-naveBProb = 0.05
-naveCProb = 0.1
+
+class naveA:
+    naves = 10
+    probabilidadPerdida = 0.1
+
+class naveB:
+    naves = 15
+    probabilidadPerdida = 0.05
+
+class naveC:
+    naves = 15
+    probabilidadPerdida = 0.1
---
+++ probabilidades.py
@@ -0,0 +1,5 @@
+from naves import *
+
+def probabilidad nave(epsilon, nave):
+    probabilidad 
+    for x in range(0, nave.naves):
